8cb89119fbeb5542198cfe57516a508fb81572bc
stakme <mail@stak.me>
2022-07-06 19:15:55 +0900

    ignore

--- script/build.sh
+++ script/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 npm i;
+npx peggy -o src/query/parse.js src/query/parse.pegjs;
+
 npx next build;
 npx next export -o docs;
 echo stak.me > docs/CNAME;
 
-npx peggy -o src/query/parse.js src/query/parse.pegjs
