fe604b8a2d4f994cb46f882d68a9c1664bd7d96e
stakme <mail@stak.me>
2022-07-08 16:31:16 +0900

    ビルド修正

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@ module.exports = {
     images: {
         domains: ["raw.githubusercontent.com"],
     },
+    experimental: {
+        images: {
+            unoptimized: true,
+        },
+    },
 };
